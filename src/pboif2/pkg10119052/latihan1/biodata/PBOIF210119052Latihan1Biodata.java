4e18186eac12d98cd7bcd21a32c31a5a94298517
Kiki <Kiki@Kiki-PC>
2020-10-17 11:37:34 +0700

    Upload Project

---
+++ src/pboif2/pkg10119052/latihan1/biodata/PBOIF210119052Latihan1Biodata.java
@@ -0,0 +1,29 @@
+
+package pboif2.pkg10119052.latihan1.biodata;
+
+/**
+ *
+ * Nama : Rizki Dwi Nugraha
+ * Kelas : IF-2
+ * NIM : 10119052
+ * 
+ */
+public class PBOIF210119052Latihan1Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "---------------------");
+        System.out.println("NAMA\t\t: Rizki Dwi Nugraha");
+        System.out.println("ALAMAT\t\t: Jalan Cirateun Peuntas, Lembang");
+        System.out.println("TEMPAT LAHIR\t: Bandung");
+        System.out.println("TANGGAL LAHIR\t: 03 Januari 2001");
+        System.out.println("---------------------");
+        System.out.println("HOBI\t\t: Gaming & Bersepeda");
+        System.out.println("MAKES\t\t: Chicken & Seblak");
+        System.out.println("MIKES\t\t: Teh Hangat");
+    }
+    
+}
